82b4c28ee5dd3eab70cf6d2152411838f82775a5
gaussmouse <98789124+gaussmouse@users.noreply.github.com>
2022-11-29 16:33:35 -0800

    Updated Routings.js
    Added semicolon; Corrected FarmPage import pathway

--- Routings.js
+++ Routings.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Wrapper from './reactComponents/Wrapper'
-import FarmPage from "./reactComponents/FarmsMap";
+import Wrapper from './reactComponents/Wrapper';
+import FarmPage from "./reactComponents/FarmPage";
 
 const Stack = createNativeStackNavigator();
 
